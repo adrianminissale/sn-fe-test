a99b8651ac6e7a1a8b3b1938f682d3b942567b58
adrian minissale <adrian.minissale@almundo.com>
2021-07-16 17:55:34 +0200

    change validation on Task 1

--- task1/index.js
+++ task1/index.js
@@ -1,19 +1,32 @@
-const operators = ['+', '−', '∗', '/', 'sin', 'cos', 'tan']
-const jsOperators = ['+', '−', '∗', '/', 'Math.sin', 'Math.cos', 'Math.tan']
 const expressions = ['3+2+4', '+2', '−2', 'sin(sin(30) + cos(20))', 'sin(30) + cos(20)', 'sin(30', '3++', '3+']
 
-const transform = (ex) => {
-  const result = operators.map( (op, i) => {
-    return ex = ex.replaceAll(op, jsOperators[i])
+const closeExpression = ex => {
+  let open = 0
+  let close = 0
+
+  ex.split('').map(e => {
+    e === '(' ? ++open : null
+    e === ')' ? ++close : null
   })
-  try {
-    return eval( result[operators.length - 1] ) && 'T'
-  } catch {
+
+  return open !== close
+}
+
+const validate = ex => {
+  // Validate operator at end
+  if ( ['+', '−', '∗', '/'].some(el => ex.split('').pop().includes(el)) )
     return 'F'
-  }
+  // Validate operator open and close
+  if ( ( ex.indexOf('sin(') === 0 || ex.indexOf('cos(') === 0 || ex.indexOf('tan(') === 0 ) && ex.split('').pop() !== ')' )
+    return 'F'
+  //
+  if ( closeExpression( ex ) )
+    return 'F'
+
+  return 'T'
 }
 
 expressions.map( ex => {
-  const result = `${ex} : ${transform( ex )}`
+  const result = `${ex} : ${validate( ex )}`
   document.getElementById( 'root' ).innerHTML += result + '</br>'
 })
